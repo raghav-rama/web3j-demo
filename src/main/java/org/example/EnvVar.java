72e7fa063831c48551332c1bf61ca0de606aee91
raghav-rama <hackerer528@gmail.com>
2022-12-11 23:44:50 +0530

    feat: add environment variables

--- src/main/java/org/example/EnvVar.java
+++ src/main/java/org/example/EnvVar.java
@@ -6,5 +6,4 @@ public class EnvVar {
   static Dotenv dotenv = Dotenv.load();
   final static String ALCHEMY_API_URL=dotenv.get("ALCHEMY_API_URL");
   final static String GOERLI_PRIVATE_KEY=dotenv.get("GOERLI_PRIVATE_KEY");
-
 }

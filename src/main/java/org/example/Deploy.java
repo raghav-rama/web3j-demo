745e95f58fac8ebf91fef97411279eec59ca9c86
raghav-rama <hackerer528@gmail.com>
2022-12-11 23:45:35 +0530

    feat: add code for deploying the contract

--- src/main/java/org/example/Deploy.java
+++ src/main/java/org/example/Deploy.java
@@ -1,12 +1,19 @@
 package org.example;
 
+import com.github.lalyos.jfiglet.FigletFont;
+import org.example.contract.wrappers.JavaToken;
+import org.web3j.crypto.Credentials;
+import org.web3j.protocol.Web3j;
+import org.web3j.tx.gas.DefaultGasProvider;
+
 public class Deploy {
-
-  public int deploy() {
+  public static int deploy(Web3j web3j) throws Exception {
+    System.out.println("Deploying on Goerli... ");
+    JavaToken javaToken = JavaToken.deploy(web3j, Credentials.create(EnvVar.GOERLI_PRIVATE_KEY), new DefaultGasProvider()).send();
+    System.out.println(FigletFont.convertOneLine("Success!"));
+    System.out.println("Deployed at: ");
+    System.out.println("https://goerli.etherscan.io/address/"+javaToken.getContractAddress());
+    System.out.println(javaToken.getTransactionReceipt());
     return 0;
-
-
   }
-
-
 }
